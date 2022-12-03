fbf4006ec269f97081656ae5db7033cb6d7bdbf1
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2022-12-03 09:50:53 -0800

    initial IAP, App Store Purchase commit

--- Sources/StoreKitted/StoreKitted.swift
+++ Sources/StoreKitted/StoreKitted.swift
@@ -1,6 +1,194 @@
-public struct StoreKitted {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import StoreKit
 
-    public init() {
+public struct StoreKitted {
+
+    public enum PurchaseError: Error {
+        case canceled
+        case errorFailure(Error)
+        case fail
+        case pending
+        case successWithConversionError
+        case verification
+        case unknown
+
+        var userInfo: [String: String] {
+            switch self {
+            case .canceled:
+                return [:]
+            case .errorFailure(let error):
+                return [NSLocalizedDescriptionKey: "\(error.localizedDescription)"]
+            case .fail, .unknown:
+                return [NSLocalizedDescriptionKey: "An unknown error occurred. Please try again later"]
+            case .pending:
+                return [NSLocalizedDescriptionKey: "Your purchase is pending. Once it's completed, your purchase will be activated"]
+            case .successWithConversionError:
+                return [NSLocalizedDescriptionKey: "Your purchase appears to have completed successfully but we're unable to verify it at this time. Once verification succeeds, your purchase will be activated. Thank you for your patience"]
+            case .verification:
+                return [NSLocalizedDescriptionKey: "Your purchase failed verification. Please try again"]
+            }
+        }
     }
+
+    var fetchedProducts: [Product] {
+        purchaseManager.fetchedProducts
+    }
+
+    var purchasedProducts: [Product] {
+        purchaseManager.purchasedProducts
+    }
+
+    private var purchaseManager: PurchaseManager
+    private var productIds: [String]
+
+    public init(productIds: [String]) {
+        self.productIds = productIds
+        self.purchaseManager = .init(productIds: productIds)
+    }
+
+    mutating public func addProductId(_ productId: String) async {
+        self.productIds.append(productId)
+        _ = try? await purchaseManager.fetchProducts()
+    }
+
+    @MainActor
+    @discardableResult mutating public func fetchProducts() async throws -> [Product] {
+        try await purchaseManager.fetchProducts()
+    }
+
+    public func requestAndHandlePurchase(_ product: Product) async -> Result<Product, PurchaseError> {
+        await purchaseManager.requestAndHandlePurchase(product)
+    }
+
+    public func restorePurchases() async throws  {
+        try await purchaseManager.fetchProducts()
+    }
+}
+
+class PurchaseManager: NSObject, ObservableObject, SKPaymentTransactionObserver {
+    private typealias Transaction = StoreKit.Transaction
+    private typealias TransactionVerificationResult = VerificationResult<Transaction>
+
+    // MARK: Init
+    static var productIds: [String] = []
+    private var updateListenerTask: Task<Void, Error>? = nil
+    init(productIds: [String]) {
+        super.init()
+        Self.productIds = productIds
+        updateListenerTask = listenForTransactions()
+        Task {
+            try? await fetchProducts()
+        }
+    }
+
+    @Published var fetchedProducts: [Product] = []
+    @Published var purchasedProducts: [Product] = []
+
+    @MainActor
+    @discardableResult func fetchProducts(processPurchasesHandler: @escaping ([Product]) async -> Void = { _ in }) async throws -> [Product] {
+        let products = try await Product.products(for: Self.productIds)
+        guard products.first != nil else { throw StoreKitted.PurchaseError.unknown }
+        self.fetchedProducts = products
+        try await checkPurchased()
+        return products
+    }
+
+    @MainActor
+    private func checkPurchased() async throws {
+        for product in fetchedProducts {
+            guard let state = await product.currentEntitlement else { continue }
+            let transaction = try self.checkVerified(state)
+            //Always finish a transaction.
+            await transaction.finish()
+        }
+    }
+
+    /// Call when the user initiates a purchase in order to handle actions (such as cancel) and errors (such as `.fail`)
+    @MainActor
+    @discardableResult func requestAndHandlePurchase(_ product: Product) async -> Result<Product, StoreKitted.PurchaseError> {
+        do {
+            let result = try await product.purchase()
+            switch result {
+            case .success(let transactionResult):
+                switch transactionResult {
+                case let .verified(transaction):
+                    guard let product = map(productId: transaction.productID) else {
+                        return .failure(.successWithConversionError)
+                    }
+                    self.purchasedProducts.append(product)
+                    return .success(product)
+                case .unverified:
+                    return .failure(.fail)
+                }
+            case .userCancelled:
+                return .failure(.canceled)
+            case .pending:
+                return .failure(.pending)
+            @unknown default:
+                return .failure(.unknown)
+            }
+        } catch {
+            return .failure(.errorFailure(error))
+        }
+    }
+
+    func restorePurchases() async throws  {
+        try await fetchProducts() { [weak self] products in
+            try? await self?.checkPurchased()
+        }
+    }
+    // MARK: Helpers
+    private func listenForTransactions() -> Task<Void, Error> {
+        return Task.detached {
+            //Iterate through any transactions that don't come from a direct call to `purchase()`.
+            for await result in Transaction.updates {
+                do {
+                    let transaction = try self.checkVerified(result)
+
+                    //Always finish a transaction.
+                    await transaction.finish()
+                } catch {
+                    //StoreKit has a transaction that fails verification. Don't deliver content to the user.
+                    throw error
+                }
+            }
+        }
+    }
+
+    private func checkVerified(_ result: TransactionVerificationResult) throws -> Transaction {
+        switch result {
+        case .verified(let transaction):
+            guard let product = map(productId: transaction.productID) else {
+                return transaction
+            }
+            self.purchasedProducts.append(product)
+        case .unverified:
+            throw StoreKitted.PurchaseError.verification
+        }
+        throw StoreKitted.PurchaseError.unknown
+    }
+
+    private func map(productId: String) -> Product? {
+        fetchedProducts.first { $0.id == productId }
+    }
+
+    deinit {
+        updateListenerTask?.cancel()
+    }
+
+    // MARK: SKPaymentQueue Listener
+    func paymentQueue(
+        _ queue: SKPaymentQueue,
+        shouldAddStorePayment payment: SKPayment,
+        for product: SKProduct
+    ) -> Bool {
+        fetchedProducts.map{ $0.id }.contains(product.productIdentifier)
+    }
+
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        Task {
+            try await fetchProducts()
+        }
+    }
+
 }
